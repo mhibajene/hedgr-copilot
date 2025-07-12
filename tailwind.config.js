1473ec8dc637eed4af0dfbe90ae6c9a19bcbcc71
mhibajene <mhibajene@gmail.com>
2025-07-12 16:57:46 +0000

    chore: configure Tailwind content paths & theme colours

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './apps/frontend/pages/**/*.{js,ts,jsx,tsx}',
+    './apps/frontend/components/**/*.{js,ts,jsx,tsx}',
+    './packages/ui/src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'hedgr-start': '#00bfa6',
+        'hedgr-middle': '#00897b',
+        'hedgr-end': '#00695c'
+      }
+    }
+  },
+  darkMode: 'class'
+}
