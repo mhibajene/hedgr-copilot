c4c8004166b68716e51aa9404250a31da64dc33c
mhibajene <mhibajene@gmail.com>
2025-07-12 14:59:35 +0000

    build (frontend): remove typescript

---
+++ apps/frontend/pages/index.js
@@ -0,0 +1,8 @@
+// apps/frontend/pages/index.js
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-purple-600 to-blue-500 flex items-center justify-center text-white">
+      <h1 className="text-4xl font-inter">Hello Hedgr</h1>
+    </div>
+  );
+}
