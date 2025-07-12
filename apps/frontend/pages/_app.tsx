508569603025dad3d4c70ec05fe98a74350ec28b
mhibajene <mhibajene@gmail.com>
2025-07-12 17:22:34 +0000

    fix(frontend): import Inter from 'next/font/google' instead of '@next/font/google

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
 // apps/frontend/pages/_app.tsx
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-import { Inter } from '@next/font/google'
+import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
